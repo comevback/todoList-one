7fe3c56fa21fc0140a96cd400638680e2aadacbe
徐响 <xuxiang@xuxiangdeMacBook-Pro.local>
2023-09-17 00:49:26 +0900

    add plan item function

--- app.js
+++ app.js
@@ -21,14 +21,16 @@ let item = await todolist.find({}, {_id:0, context:1});
 
 item.forEach((item) => {
     todoArr.push(item.context);
-})
+});
 let index = todoArr.length + 1;
 
 app.get("/", (req, res) => {
     res.render("index.ejs", {
         items: todoArr
     });
-})
+});
+
+//await todolist.deleteMany({_id:{$gt:3}});
 
 app.post("/add", async(req, res) => {
     const input = req.body.input; 
@@ -37,10 +39,17 @@ app.post("/add", async(req, res) => {
         context: input
     })
     index += 1;
-    todolist.insertMany([newItem]).then(console.log("new item was added."));
+    todolist.insertMany([newItem]).then(console.log(`new item (${input}) was added.`));
+    item = await todolist.find({}, { _id: 0, context: 1 });
+    todoArr = item.map((item) => item.context);
+    console.log(`now the to-do-list has ${todoArr.length} items.`)
     res.redirect("/");
-})
+});
+
+app.delete("/delete", (req, res) => {
+    
+});
 
 app.listen(port, ()=> {
     console.log(`Server is listening on port ${port}.`);
-})
+});
