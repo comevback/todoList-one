614510de9d526efac1fd345fbb2a6d0dfe19ab90
徐响 <xuxiang@xuxiangdeMacBook-Pro.local>
2023-09-26 19:07:31 +0900

    customList creating and adding deleting

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
+import { render } from "ejs";
 
 const app = express();
 const port = 3000;
@@ -9,32 +10,29 @@ const mongoUrl = "mongodb://localhost:27017/todoList"
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect(mongoUrl).then(console.log("Successfully Connected to the Database!"));
+mongoose.connect(mongoUrl).then(console.log("Successfully Connected to the MongoDB!"));
 const todoSchema = new mongoose.Schema({
-    _id: Number,
     context: String
 });
-//创建items的模版
+//模版
 
-// const collectionSchema = new mongoose.Schema({
-//     name: String,
-//     items: [todoSchema]
-// });
-//创建collection的模版
+const listSchema = new mongoose.Schema({
+    name: String,
+    items: [todoSchema]
+});
+//创建List的模版
 
-const todolist = mongoose.model("todolist", todoSchema); //这是通过items模版，创建collection里的一个items。
-//const collection = mongoose.model("collection", collectionSchema);//这是通过collection模版，创建database里的一个collection。
+const todolist = mongoose.model("todolist", todoSchema); //范例
+const List = mongoose.model("List", listSchema);//这是通过List模版，创建database里的一个List。
 
-//let todoArr = [];
-let items = await todolist.find({}, {_id:0, context:1});
+let items = await todolist.find({}, { context:1 });
+let now = new Date().toLocaleDateString(undefined,{ weekday: 'long' , year: 'numeric' , month: 'long' , day: 'numeric'
+});
 
-/*item.forEach((item) => {
-    todoArr.push(item.context);
-});*/
-let index = items.length + 1;
 
 app.get("/", (req, res) => {
     res.render("index.ejs", {
+        title: now,
         items: items
     });
 });
@@ -43,35 +41,88 @@ app.get("/", (req, res) => {
 
 app.post("/add", async(req, res) => {
     const input = req.body.input; 
+    const listname = req.body.listName;
     const newItem = new todolist({
-        _id: index,
         context: input
     })
-    index += 1;
-    todolist.insertMany([newItem]).then(console.log(`New item (${input}) was added.`));
-    items = await todolist.find({}, { _id: 0, context: 1 });
-    console.log(`now the to-do-list has ${items.length} items.`)
-    res.redirect("/");
+    //实例
+    if(listname === now){
+        todolist.insertMany([newItem])
+        .then(console.log(`New item (${input}) was added.`));
+        items = await todolist.find({}, { context: 1 });
+        console.log(`now the to-do-list has ${items.length} items.`)
+        res.redirect("/");
+    }else{
+        List.findOne({name:listname})
+        .then((foundList) => {
+            foundList.items.push(newItem);
+            foundList.save();
+            res.redirect("/" + listname);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    }
 });
 
 app.post("/delete", async(req, res) => {
-    const checkedItem = req.body.checkbox;
-    console.log(checkedItem);
-    todolist.deleteOne({context: checkedItem}).then(console.log(`Item (${checkedItem}) was deleted.`));
-    items = await todolist.find({}, { _id: 0, context: 1 });
-    console.log(`now the to-do-list has ${items.length} items.`)
-    res.redirect("/");
-});
+    const checkedItemId = req.body.checkbox;
+    const listname = req.body.listName;
+    console.log(checkedItemId);
 
-app.listen(port, ()=> {
-    console.log(`Server is listening on port ${port}.`);
+    if(listname === now){
+        todolist.findByIdAndRemove(checkedItemId)
+        .then(
+            console.log(`Item (${checkedItemId}) was deleted from todolist.`)
+        )
+        .catch((err) => {
+            console.log(err);
+        });
+        items = await todolist.find({}, { _id: 1, context: 1 });
+        console.log(`now the to-do-list has ${items.length} items.`)
+        res.redirect("/");
+    }else{
+        List.findOneAndUpdate({name: listname}, {$pull:{items:{_id: checkedItemId}}}) // use $pull to delete item from items of lists.
+        .catch((err) => {
+            console.log(err);
+        })
+        res.redirect("/" + listname);
+    }
+    // another way:
+    // todolist.deleteOne({_id: checkedItemId})
+    // .then(console.log(`Item (${checkedItemId}) was deleted.`))
+    // .catch((err) => {
+    //      console.log(err);
+    //  });
 });
 
 app.get("/:customList", async(req, res) => {
     const customList = req.params.customList;
     
-    
-    res.render("index.ejs", {
-        items: customitems
-    });
-})
+    try{
+        const foundList = await List.findOne({name: customList});
+        if(!foundList){
+            const list = new List({
+                name: customList,
+                items: []
+            })
+            //List.insertMany([collect]).then(console.log(`New list (${collect}) was added to List`));
+            await list.save();
+            console.log("New list was added to List:", list);
+            res.redirect("/" + customList);
+        }else{
+        res.render("index.ejs", {
+            title: foundList.name,
+            items: foundList.items
+        });
+        }
+    }catch(err){
+        console.log(err);
+    }
+});
+  
+
+app.listen(port, ()=> {
+    console.log(`Server is listening on port ${port}.`);
+});
+
